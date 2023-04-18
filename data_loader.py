f2055ba78c2a7810fa85465acd886a81beee668f
Hwi3 <e0758848@u.nus.edu>
2023-04-18 17:29:00 +0900

    0418

--- data_loader.py
+++ data_loader.py
@@ -45,20 +45,5 @@ def load_transformed_dataset():
 
     train, test = torch.utils.data.random_split(dataset,[7000,220])
 
-
     return torch.utils.data.ConcatDataset([train, test])
-
-def show_tensor_image(image):
-    reverse_transforms = transforms.Compose([
-        transforms.Lambda(lambda t: (t + 1) / 2),
-        transforms.Lambda(lambda t: t.permute(1, 2, 0)), # CHW to HWC
-        transforms.Lambda(lambda t: t * 255.),
-        transforms.Lambda(lambda t: t.numpy().astype(np.uint8)),
-        transforms.ToPILImage(),
-    ])
-
-    # Take first image of batch
-    if len(image.shape) == 4:
-        image = image[0, :, :, :] 
-    plt.imshow(reverse_transforms(image))
 
